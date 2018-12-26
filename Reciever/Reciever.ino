845616c0ce0013add15aca41094aef12386e6c1a
NotD4vid <41587297+NotD4vid@users.noreply.github.com>
2018-12-26 11:05:58 +0000

    Debugged everything sort of works

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,7 +1,7 @@
 /*
-SimpleReceive
-This sketch displays text strings received using VirtualWire
-Connect the Receiver data pin to Arduino pin 11
+  SimpleReceive
+  This sketch displays text strings received using VirtualWire
+  Connect the Receiver data pin to Arduino pin 11
 */
 #define whiteLed 3
 #define greenLed 4
@@ -11,6 +11,7 @@ int loopTime = 0;
 int detectionConstant = 3000;
 int timer = 0;
 int c = 0;
+bool shotTrig = false;
 bool r = false;
 byte message[VW_MAX_MESSAGE_LEN]; // a buffer to store the incoming messages
 byte messageLength = VW_MAX_MESSAGE_LEN; // the size of the message
@@ -19,71 +20,78 @@ NewPing sonar(9, 10, 100);
 
 void setup()
 {
-Serial.begin(9600);
-Serial.println("Device is ready");
+  Serial.begin(9600);
+  Serial.println("Device is ready");
 
-vw_setup(2000); // Bits per sec
-vw_rx_start(); // Start the receiver
-pinMode(whiteLed,OUTPUT);
-pinMode(greenLed,OUTPUT);
+  vw_setup(2000); // Bits per sec
+  vw_rx_start(); // Start the receiver
+  pinMode(whiteLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
 
 }
 
 
 void loop()
 {
-Serial.println(sonar.ping_cm());
 
-if (sonar.ping_cm()==0)
-{
-    pinMode(10, OUTPUT);   
+
+  if (sonar.ping_cm() == 0)
+  {
+    pinMode(10, OUTPUT);
     digitalWrite(10, LOW);
     pinMode(10, INPUT);
-    
-}
 
-if (vw_get_message(message, &messageLength)) // Non-blocking
-{
-Serial.print("Received: ");
+  }
 
-
-c=message[0];
-Serial.println(c);
-}
-
-
-if (c == 50)
-{
-  timer = millis();
-  loopTime = 0;
-  digitalWrite(whiteLed,HIGH);
-  while (loopTime<detectionConstant)
+  if (vw_get_message(message, &messageLength)) // Non-blocking
   {
-    if ((sonar.ping_cm()<10)and (sonar.ping_cm()>0))
+    Serial.print("Received: ");
+
+
+    c = message[0];
+    Serial.println(c);
+  }
+
+
+  if (c == 50)
+  {
+    timer = millis();
+    loopTime = 0;
+    digitalWrite(whiteLed, HIGH);
+    Serial.println("before while");
+    while (loopTime < detectionConstant)
     {
-      digitalWrite(greenLed,HIGH);
-      delay(1000);
-      digitalWrite(whiteLed,LOW);
-      digitalWrite(greenLed,LOW);
+
+      if ((sonar.ping_cm() < 10) and (shotTrig == false) and (sonar.ping_cm() > 0) )
+      {
+        shotTrig = true;
+        Serial.println("before green:");
+        Serial.println(sonar.ping_cm());
+        digitalWrite(greenLed, HIGH);
+        delay(detectionConstant);
+        digitalWrite(whiteLed, LOW);
+        digitalWrite(greenLed, LOW);
+
+      }
+
+      loopTime = (millis() - timer);
+      Serial.println(loopTime);
+
     }
-    else
-    loopTime = (millis()-timer);
-   
-   
+    if (loopTime > (detectionConstant-1))
+    {
+      digitalWrite(whiteLed, LOW);
+      c = 0;
+      shotTrig = false;
+    }
+    c = 0;
   }
-  if (loopTime>detectionConstant)
+
+  else
   {
-    digitalWrite(whiteLed,LOW);
-    c=0;
+    c = 0;
+
   }
-  c=0;
-}
-
-else
-{ 
-c=0;
-
-}
 
 
 
